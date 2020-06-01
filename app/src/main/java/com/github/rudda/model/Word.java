15930452d14cd574a41b160339412671e22a364c
Ruddá Beltrão <beltrao.rudah@gmail.com>
2020-06-01 04:18:40 -0400

    feat: searching is works

--- app/src/main/java/com/github/rudda/model/Word.java
+++ app/src/main/java/com/github/rudda/model/Word.java
@@ -1,5 +1,9 @@
 package com.github.rudda.model;
 
+import android.util.Log;
+
+import java.util.HashMap;
+
 public class Word {
     private String word;
 
@@ -8,18 +12,22 @@ public class Word {
     }
 
     public int jumbledLetters(String pseudo, int length) {
+        //length = this.word.length();
         int jumbled_count = 0;
         //let isNotjumbled_count = 0;
 
         if (this.word.charAt(0) == pseudo.charAt(0)) {
-            jumbled_count ++;
-            this.word = this.word.substring(1);
+            jumbled_count+= 1;
+            String w;
+            w = this.word.substring(1);
             pseudo = pseudo.substring(1);
-
-            for(int i= 0;  i < length ; i++) {
-                boolean isJumbled = this.isJumbled(word.substring(i), pseudo.substring(i));
-                if(isJumbled) {
-                    jumbled_count ++;
+            Log.i("aqui", w.length() +" " + pseudo);
+            for(int i= 0;  i < (w.length() -1); i++) {
+                boolean _isJumbled = this.isJumbled(w.substring(i), pseudo.substring(i));
+                Log.i("aqui", "isJumbled " + _isJumbled );
+                if(_isJumbled) {
+                    jumbled_count += 1;
+                    Log.i("aqui", " jumbled_count " +jumbled_count);
                 }
             }
         }
@@ -27,6 +35,7 @@ public class Word {
     }
 
     private boolean isJumbled(String str, String str2) {
+        Log.i("aqui", "str1 "+str +" str2 "+ str2);
         if(str.charAt(0) == str2.charAt(0)){
             return false;
         } else if (str2.charAt(1) == str.charAt(0) && str2.charAt(0) == str.charAt(1)) {
@@ -41,5 +50,70 @@ public class Word {
 
     public void setWord(String word) {
         this.word = word;
+    }
+
+    private void register(int index,  char my_char, boolean match, HashMap<Integer, WordMatch > map) {
+        map.put(index, new WordMatch(my_char, match));
+    }
+
+    public boolean isTypo (String str1, String str2) {
+        HashMap<Integer, WordMatch > char_map = new HashMap<>();
+        int str1_index= str1.length();
+        int str2_index= str2.length();
+        int j=0, j_index = 0;
+        boolean match= false; int typo=0;
+
+        for(int i=0; i< str1_index; i++) {
+            j_index = j;
+
+            if (j == str2_index) {
+                match = false;
+                register(i, str1.charAt(i), match, char_map) ;
+            }
+            for(;j<str2_index; j++){
+
+                if(str1.charAt(i) == str2.charAt(j)) {
+                    match = true;
+                    register(i, str1.charAt(i), match, char_map) ;
+                    j++;
+                    break;
+                } else {
+                    match = false;
+                    register(i, str1.charAt(i), match, char_map) ;
+                }
+
+            }
+
+            j= !match ?  j_index : j;
+            typo= (char_map.get(i).isStatus()) == false ? typo += 1: typo;
+        }
+
+        return typo <= 1;
+    }
+
+    class WordMatch {
+        char value;
+        boolean status;
+
+        public WordMatch(char value, boolean status) {
+            this.value = value;
+            this.status = status;
+        }
+
+        public char getValue() {
+            return value;
+        }
+
+        public void setValue(char value) {
+            this.value = value;
+        }
+
+        public boolean isStatus() {
+            return status;
+        }
+
+        public void setStatus(boolean status) {
+            this.status = status;
+        }
     }
 }
--- app/src/main/java/com/github/rudda/view/activities/MainActivity.java
+++ app/src/main/java/com/github/rudda/view/activities/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.SearchView;
 
 import com.github.rudda.R;
 import com.github.rudda.model.Fruit;
+import com.github.rudda.model.Word;
 import com.github.rudda.model.adapters.FruitAdapter;
 
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     private SearchView mSearchView;
     private RecyclerView.LayoutManager lm;
     private FruitAdapter mAdapter;
+    private Fruit mFruitList = new Fruit();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +31,14 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     private void init(){
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-        mRecyclerViewList.setHasFixedSize(true);
+       // mRecyclerViewList.setHasFixedSize(true);
 
         // use a linear layout manager
         lm = new LinearLayoutManager(this);
         mRecyclerViewList.setLayoutManager(lm);
 
         // specify an adapter (see also next example)
-        mAdapter = new FruitAdapter(new Fruit());
+        mAdapter = new FruitAdapter(mFruitList);
         mRecyclerViewList.setAdapter(mAdapter);
     }
 
@@ -49,12 +51,52 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     @Override
     public boolean onQueryTextSubmit(String query) {
         Log.i("onQueryTextSubmit", query);
+        Fruit f = new Fruit();
+        f.getFruits().clear();
+
+//        for (int i = 0; i < this.mFruitList.getFruits().size(); i++) {
+        for (int i = 0; i < 10; i++) {
+        Word w = this.mFruitList.getFruits().get(i);
+            int result_1= 0;
+            boolean result_2 = false;
+             if(  w.getWord().length() == query.length()){
+                 result_1 = w.jumbledLetters(query, w.getWord().length());
+             } else {
+                 result_2 = w.isTypo(w.getWord(), query);
+
+             }
+
+            if( result_1 > 1 && !result_2) {
+                Log.i("Im", "a jumbled "+w.getWord());
+                f.getFruits().add(w);
+            } else if( result_2 && !(result_1 >1)) {
+                Log.i("Im", "a typo "+w.getWord());
+                f.getFruits().add(w);
+            }
+
+        }
+
+        //this.mFruitList = f;
+        this.mAdapter = new FruitAdapter(f);
+        this.mRecyclerViewList.setAdapter(this.mAdapter);
+        Log.i("onQueryTextSubmit", mFruitList.getFruits().size() + "---" + f.getFruits().size());
+        this.mAdapter.notifyDataSetChanged();
         return false;
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        Log.i("onQueryTextChange", newText);
+
+//       // for (int i = 0; i < this.mFruitList.getFruits().size(); i++) {
+//            Word w = this.mFruitList.getFruits().get(0);
+//
+//            Log.i("onQueryTextChange", "--> "+w.jumbledLetters("yuo", w.getWord().length()));
+////            if(  w.jumbledLetters(newText, w.getWord().length()) > 1){
+////                Log.i("onQueryTextChange", "maior q 1");
+////            }
+//
+////        }
+//        Log.i("onQueryTextChange", newText);
         return false;
     }
 }
